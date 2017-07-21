163d229a673a6f6b65f14d711cf5cb85367b1be9
Roshan <troshan23@gmail.com>
2017-07-21 17:01:04 +0530

    MailSystemCommited

---
+++ EmailSystem/EMailMngSys/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EMailMngSys.Models;
+
+namespace EMailMngSys.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index(UserDetails model)
+        {
+            //model.Name = "Roshan Thakur";
+            //model.Address = "1546,Arthala Mohan Nagar Ghazibad";
+            //model.Contact = "9716526132";
+            //model.Email = "troshan23@gmail.com";
+            //model.Password = "1234567";
+
+            return View(model);
+        }
+	}
+}
